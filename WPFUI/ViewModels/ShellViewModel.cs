43ea4c0e7cbf4a35aface7d6c5eca50c99674803
Scott W. Vincent <scott@swvincent.com>
2023-02-07 06:13:16 -0500

    FullName

--- WPFUI/ViewModels/ShellViewModel.cs
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -10,11 +10,34 @@ namespace WPFUI.ViewModels
     public class ShellViewModel : Screen
     {
 		private string _firstName = "Scott";
+        private string _lastName;
 
-		public string FirstName
+        public string FirstName
 		{
 			get { return _firstName; }
-			set { _firstName = value; }
+			set
+			{ 
+				_firstName = value;
+				NotifyOfPropertyChange(() => FirstName);
+                NotifyOfPropertyChange(() => FullName);
+            }
 		}
+
+		public string LastName
+		{
+			get { return _lastName; }
+			set 
+			{ 
+				_lastName = value; 
+				NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+		}
+
+		public string FullName
+		{
+			get { return $"{FirstName} {LastName}"; }
+		}
+
 	}
 }
