068ec2936ec5aadf9f120997bf56bb4359720509
Scott W. Vincent <scott@swvincent.com>
2023-02-06 20:21:51 -0500

    Add Boostrapper and remove default MainWindow

---
+++ WPFUI/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //TODO:DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
